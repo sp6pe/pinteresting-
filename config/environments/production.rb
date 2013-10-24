682db632d6664417dd9107143b32eef7c8707724
sp6pe <sp6pe@virginia.edu>
2013-10-23 21:19:21 -0400

    Implemented devise for users

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,9 @@ Pinteresting::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  #Required for Heroku
+  #Note to set to actual host name 
+  config.action_mailer.default_url_options = { :host => 'jai-app.herokuapp.com' }
+
 end
